ea2b4d158d75fa70ff99af618427e68fcf51a8b7
Dominic Lewis <domlewis@cisco.com>
2019-01-28 23:20:07 +0000

    Working rough script, will need to replace the default file scripts when have access to actual machine

--- fix_var.sh
+++ fix_var.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
+echo "CLOSE ALL OPEN ADOBE SOFTWARE"
+DEFAULT_PATH="example_text.md"
+FILE_PATH=${1:-$DEFAULT_PATH}
+echo $FILE_PATH
 
+sudo sed s/604800000/604800/g $FILE_PATH > "$FILE_PATH".tmp
 
-
-
-main(){
-    echo "Hello World"
-}
-
-main
+mv "$FILE_PATH".tmp $FILE_PATH
