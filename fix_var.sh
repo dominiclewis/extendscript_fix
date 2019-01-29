7039fd6139de1cb1cd1e40a603b5140e3774b2a9
Dominic Lewis <domlewis@cisco.com>
2019-01-29 09:59:24 +0000

    Added pause in and made more descriptive output line

--- fix_var.sh
+++ fix_var.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 echo "CLOSE ALL OPEN ADOBE SOFTWARE"
+read
 DEFAULT_PATH="example_text.md"
 FILE_PATH=${1:-$DEFAULT_PATH}
-echo $FILE_PATH
+echo "Attempting with $FILE_PATH as file path"
 
 sudo sed s/604800000/604800/g $FILE_PATH > "$FILE_PATH".tmp
 
