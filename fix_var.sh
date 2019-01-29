86f3a252a738b7ed480a62f5478032d39e857e64
Dominic Lewis <domlewis@cisco.com>
2019-01-29 20:32:15 +0000

    V1.0 Code Complete

--- fix_var.sh
+++ fix_var.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 echo "CLOSE ALL OPEN ADOBE SOFTWARE"
 read
-DEFAULT_PATH="/Applications/Adobe\ ExtendScript\ Toolkit\ CC/ExtendScript\ Toolkit.app/Contents/SharedSupport/Required/cdic/11BTBackend.jsx"
-FILE_PATH=${1:-$DEFAULT_PATH}
-echo "Attempting with $FILE_PATH as file path"
+EXTENSION=".jsx"
+TP=".tmp"
+FILE_NAME="11BTBackend"
+DEFAULT_DIRECTORY="/Applications/Adobe ExtendScript Toolkit CC/ExtendScript Toolkit.app/Contents/SharedSupport/Required/cdic/"
+DIREC=${1:-$DEFAULT_DIRECTORY}
+echo "Attempting with $DIREC$FILE_NAME$EXTENSION' as file path"
 
-sudo sed s/604800000/604800/g $FILE_PATH > "$FILE_PATH".tmp
+sudo sed s/604800000/604800/g "$DIREC$FILE_NAME$EXTENSION" > "$DIREC$FILE_NAME$TP"
 
-mv "$FILE_PATH".tmp $FILE_PATH
+mv "$DIREC$FILE_NAME$TP" "$DIREC$FILE_NAME$EXTENSION"
+
+echo "Script Finished"
